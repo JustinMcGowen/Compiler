e58b5f1a663994fc1c693462d801dd21ae8a3748
Justin McGowen <JMPanthers15@gmail.com>
2020-02-11 23:49:30 -0700

    file read

--- Step1/lexerMain.java
+++ Step1/lexerMain.java
@@ -1,6 +1,21 @@
+import java.io.*;
+
 public class lexerMain{
 
-  public lexerMain(){
-    
+  public static void main(String[] args)throws Exception{
+    System.out.print("Starting Lexer Script\n");
+
+    File file = new File("./inputs/fibonacci.micro");
+
+    BufferedReader br = new BufferedReader(new FileReader(file));
+
+    String program = "";
+    String st;
+    while ((st = br.readLine()) != null) {
+      program = program + st;
+      //System.out.println(st);
+    }
+    System.out.println(program);
   }
+
 }
